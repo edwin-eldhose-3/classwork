8efa4bbb8d5db06524befbc29fa683b6bde6ffe7
edwin-eldhose-3 <113668241+edwin-eldhose-3@users.noreply.github.com>
2022-09-16 14:58:06 +0530

    Add files via upload

---
+++ lecture1.py
@@ -0,0 +1,20 @@
+'''
+pokelist=[12,13,14,'jigglypuff',"1","2"]
+print(pokelist)
+for poke in pokelist:
+    print(poke)
+poke1="metagross"
+for i in poke1:
+    print(i)
+
+
+
+poke = 'pikachu'
+print(poke[4::])
+'''
+l=['bulbasaur_21','squirtle_42','charmander_09','groudon_69','seviper_68','paras_00']
+l2=[]
+for i in l:
+    if i[0] == 's':
+        l2.append(i)
+print(l2)
